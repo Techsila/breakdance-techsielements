9e83901112dfb85607136d7a2fcb7784de307500
Techsila <silje.selina@techsila.net>
2025-06-21 23:05:50 +0200

    fix

--- breakdance-techsielements.php
+++ breakdance-techsielements.php
@@ -1,13 +1,16 @@
 <?php
-/**
- * Plugin Name: Breakdance TechsiElements
- * Plugin URI: https://techsila.net
- * Description: Boilerplate plugin to save your custom elements created with Element Studio.
- * Author: Techsila
- * Author URI: https://techsila.net
- * License: GPLv2
- * Text Domain: techsila
- * Domain Path: /languages/
- * Version: 0.0.1
- * GitHub Plugin URI: https://github.com/Techsila/breakdance-techsielements
- */
+/*
+Plugin Name: Breakdance TechsiElements
+Plugin URI: https://techsila.net
+Description: Boilerplate plugin to save your custom elements created with Element Studio.
+Author: Techsila
+Author URI: https://techsila.net
+License: GPLv2
+Text Domain: techsila
+Domain Path: /languages/
+Version: 0.0.1
+GitHub Plugin URI: https://github.com/Techsila/breakdance-techsielements
+GitHub Branch: main
+*/
+
+require_once __DIR__ . '/plugin.php';
--- plugin.php
+++ plugin.php
@@ -1,18 +1,5 @@
 <?php
 
-/**
- * Plugin Name: Breakdance TechsiElements
- * Plugin URI: https://techsila.net
- * Description: Boilerplate plugin to save your custom elements created with Element Studio.
- * Author: Techsila
- * Author URI: https://techsila.net
- * License: GPLv2
- * Text Domain: techsila
- * Domain Path: /languages/
- * Version: 0.0.1
- * GitHub Plugin URI: https://github.com/Techsila/breakdance-techsielements
- */
-
 namespace BreakdanceCustomElements;
 
 use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
@@ -31,7 +18,7 @@ add_action('breakdance_loaded', function () {
         'TechsiCustom',
         'macro',
         'TechsiMacros',
-        false,
+        false
     );
 
     \Breakdance\ElementStudio\registerSaveLocation(
@@ -39,9 +26,6 @@ add_action('breakdance_loaded', function () {
         'TechiCustom',
         'preset',
         'TechiPresets',
-        false,
+        false
     );
-},
-    // register elements before loading them
-    9
-);
+}, 9);
