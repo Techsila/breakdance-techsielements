ea72787d03cba9c8cf345e933c46966c9605137d
Techsila <silje.selina@techsila.net>
2025-06-22 13:05:22 +0200

    Update

--- breakdance-techsielements.php
+++ breakdance-techsielements.php
@@ -8,7 +8,7 @@ Author URI: https://techsila.net
 License: GPLv2
 Text Domain: techsila
 Domain Path: /languages/
-Version: 0.0.3
+Version: 0.0.4
 GitHub Plugin URI: https://github.com/Techsila/breakdance-techsielements
 GitHub Branch: main
 */
