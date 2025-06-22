b48539135828b67b8e7c8432c60b9bf25147ff2b
Techsila <silje.selina@techsila.net>
2025-06-22 13:04:05 +0200

    Update

--- elements/Price_List_Repeater/element.php
+++ elements/Price_List_Repeater/element.php
@@ -5,7 +5,6 @@ namespace TechsiCustom;
 use function Breakdance\Elements\c;
 use function Breakdance\Elements\PresetSections\getPresetSection;
 
-
 \Breakdance\ElementStudio\registerElementForEditing(
     "TechsiCustom\\PriceListRepeater",
     \Breakdance\Util\getdirectoryPathRelativeToPluginFolder(__DIR__)
@@ -15,352 +14,62 @@ class PriceListRepeater extends \Breakdance\Elements\Element
 {
     static function uiIcon()
     {
-        return '<svg aria-hidden="true" focusable="false"   class="svg-inline--fa fa-square-dollar" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M228.4 240.6L220 238.2C182.7 227.2 173.3 219.1 176.6 200.4c3.75-21.88 33.44-27.31 61.25-22.94c7.625 1.188 16.81 3.703 28.94 7.891c8.375 2.891 17.47-1.547 20.34-9.891c2.875-8.359-1.531-17.47-9.906-20.36c-13.94-4.812-24.88-7.766-34.44-9.266C241.8 145.7 240.9 145.7 240 145.6V112C240 103.2 232.8 96 224 96S208 103.2 208 112v32.71C173.1 148.2 149.1 166.4 145 194.9C136.1 246.9 184.8 261.2 211 268.9l8.562 2.469c42.75 12.22 55.47 19.38 51.84 40.28c-3.75 21.91-33.44 27.27-61.34 22.92C198.3 332.8 184.3 327.8 172 323.3l-6.656-2.375C157 318 147.9 322.3 144.9 330.7c-2.938 8.344 1.438 17.48 9.75 20.42l6.5 2.328c13.72 4.953 29.28 10.55 44.06 12.78C206.2 366.3 207 366.3 208 366.4V400c0 8.844 7.156 16 16 16s16-7.156 16-16v-32.74c34.04-3.52 58.02-21.65 62.94-50.18C312.1 264.5 263.7 250.7 228.4 240.6zM384 32H64C28.65 32 0 60.66 0 96v320c0 35.34 28.65 64 64 64h320c35.35 0 64-28.66 64-64V96C448 60.66 419.3 32 384 32zM416 416c0 17.64-14.36 32-32 32H64c-17.64 0-32-14.36-32-32V96c0-17.64 14.36-32 32-32h320c17.64 0 32 14.36 32 32V416z"></path></svg>';
+        return '<svg aria-hidden="true" focusable="false" class="svg-inline--fa fa-square-dollar" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="..."></path></svg>';
     }
 
-    static function tag()
-    {
-        return 'div';
-    }
+    static function tag() { return 'div'; }
 
-    static function tagOptions()
-    {
-        return [];
-    }
+    static function tagOptions() { return []; }
 
-    static function tagControlPath()
-    {
-        return false;
-    }
+    static function tagControlPath() { return false; }
 
-    static function name()
-    {
-        return 'Price List';
-    }
+    static function name() { return 'Price List'; }
 
-    static function className()
-    {
-        return 'bde-price-list';
-    }
+    static function className() { return 'bde-price-list'; }
 
-    static function category()
-    {
-        return 'blocks';
-    }
+    static function category() { return 'blocks'; }
 
-    static function badge()
-    {
-        return false;
-    }
+    static function badge() { return false; }
 
-    static function slug()
-    {
-        return __CLASS__;
-    }
+    static function slug() { return __CLASS__; }
 
-    static function template()
-    {
+    static function template() {
         return file_get_contents(__DIR__ . '/html.twig');
     }
 
-    static function defaultCss()
-    {
+    static function defaultCss() {
         return file_get_contents(__DIR__ . '/default.css');
     }
 
-    static function defaultProperties()
-    {
+    static function cssTemplate() {
+        return file_get_contents(__DIR__ . '/css.twig');
+    }
+
+    static function defaultProperties() {
         return false;
     }
 
-    static function defaultChildren()
-    {
+    static function defaultChildren() {
         return false;
     }
 
-    static function cssTemplate()
-    {
-        $template = file_get_contents(__DIR__ . '/css.twig');
-        return $template;
-    }
-
-    static function designControls()
-    {
+    static function designControls() {
         return [c(
-        "rows",
-        "Rows",
-        [c(
-        "background_a",
-        "Background A",
-        [],
-        ['type' => 'color', 'layout' => 'inline', 'colorOptions' => ['type' => 'solidAndGradient']],
-        false,
-        false,
-        [],
-      ), c(
-        "background_b",
-        "Background B",
-        [],
-        ['type' => 'color', 'layout' => 'inline', 'colorOptions' => ['type' => 'solidAndGradient']],
-        false,
-        false,
-        [],
-      ), getPresetSection(
-      "EssentialElements\\spacing_padding_all",
-      "Padding",
-      "padding",
-       ['type' => 'popout']
-     ), c(
-        "border",
-        "Border",
-        [],
-        ['type' => 'toggle', 'layout' => 'inline'],
-        false,
-        false,
-        [],
-      ), c(
-        "border_color",
-        "Border Color",
-        [],
-        ['type' => 'color', 'layout' => 'inline'],
-        false,
-        false,
-        [],
-      )],
-        ['type' => 'section'],
-        false,
-        false,
-        [],
-      ), c(
-        "typography",
-        "Typography",
-        [getPresetSection(
-      "EssentialElements\\typography",
-      "Title",
-      "title",
-       ['type' => 'popout']
-     ), getPresetSection(
-      "EssentialElements\\typography",
-      "Price",
-      "price",
-       ['type' => 'popout']
-     ), getPresetSection(
-      "EssentialElements\\typography",
-      "Description",
-      "description",
-       ['type' => 'popout']
-     )],
-        ['type' => 'section'],
-        false,
-        false,
-        [],
-      ), c(
-        "container",
-        "Container",
-        [c(
-        "width",
-        "Width",
-        [],
-        ['type' => 'unit', 'layout' => 'inline'],
-        true,
-        false,
-        [],
-      ), c(
-        "background",
-        "Background",
-        [],
-        ['type' => 'color', 'layout' => 'inline', 'colorOptions' => ['type' => 'solidAndGradient']],
-        false,
-        false,
-        [],
-      ), getPresetSection(
-      "EssentialElements\\borders",
-      "Borders",
-      "borders",
-       ['type' => 'popout']
-     ), c(
-        "shadow",
-        "Shadow",
-        [],
-        ['type' => 'shadow', 'layout' => 'vertical'],
-        false,
-        false,
-        [],
-      ), getPresetSection(
-      "EssentialElements\\spacing_padding_all",
-      "Padding",
-      "padding",
-       ['type' => 'popout']
-     )],
-        ['type' => 'section'],
-        false,
-        false,
-        [],
-      ), getPresetSection(
-      "EssentialElements\\spacing_margin_y",
-      "Spacing",
-      "spacing",
-       ['type' => 'popout']
-     )];
-    }
-
-    static function contentControls()
-    {
-        return [c(
-        "content",
-        "Content",
-        [c(
-        "items",
-        "Items",
-        [c(
-        "title",
-        "Title",
-        [],
-        ['type' => 'text', 'layout' => 'vertical'],
-        false,
-        false,
-        [],
-      ), c(
-        "description",
-        "Description",
-        [],
-        ['type' => 'text', 'layout' => 'vertical'],
-        false,
-        false,
-        [],
-      ), c(
-        "price",
-        "Price",
-        [c(
-        "price",
-        "Price",
-        [],
-        ['type' => 'text', 'layout' => 'vertical'],
-        false,
-        false,
-        [],
-      ), c(
-        "currency_symbol",
-        "Currency Symbol",
-        [],
-        ['type' => 'text', 'layout' => 'vertical'],
-        false,
-        false,
-        [],
-      ), c(
-        "currency_order",
-        "Currency order",
-        [],
-        ['type' => 'dropdown', 'layout' => 'vertical', 'items' => [['value' => 'before', 'text' => 'Before'], ['text' => 'After', 'value' => 'after']], 'placeholder' => 'Before'],
-        false,
-        false,
-        [],
-      ), c(
-        "add_",
-        "Add ,-",
-        [],
-        ['type' => 'toggle', 'layout' => 'vertical'],
-        false,
-        false,
-        [],
-      ), c(
-        "unit",
-        "Unit",
-        [],
-        ['type' => 'text', 'layout' => 'vertical'],
-        false,
-        false,
-        [],
-      )],
-        ['type' => 'section', 'layout' => 'vertical'],
-        false,
-        false,
-        [],
-      )],
-        ['type' => 'repeater', 'layout' => 'vertical', 'repeaterOptions' => ['titleTemplate' => '{item}', 'defaultTitle' => 'Item', 'buttonName' => 'Add item']],
-        false,
-        false,
-        [],
-      )],
-        ['type' => 'section', 'layout' => 'vertical'],
-        false,
-        false,
-        [],
-      )];
-    }
-
-    static function settingsControls()
-    {
-        return [];
-    }
-
-    static function dependencies()
-    {
-        return false;
-    }
-
-    static function settings()
-    {
-        return ['proOnly' => true];
-    }
-
-    static function addPanelRules()
-    {
-        return false;
-    }
-
-    static public function actions()
-    {
-        return false;
-    }
-
-    static function nestingRule()
-    {
-        return ["type" => "final",   ];
-    }
-
-    static function spacingBars()
-    {
-        return [['location' => 'outside-top', 'cssProperty' => 'margin-top', 'affectedPropertyPath' => 'design.spacing.margin_top.%%BREAKPOINT%%'], ['location' => 'outside-bottom', 'cssProperty' => 'margin-bottom', 'affectedPropertyPath' => 'design.spacing.margin_bottom.%%BREAKPOINT%%']];
-    }
-
-    static function attributes()
-    {
-        return false;
-    }
-
-    static function experimental()
-    {
-        return false;
-    }
-
-    static function order()
-    {
-        return 750;
-    }
-
-    static function dynamicPropertyPaths()
-    {
-        return [];
-    }
-
-    static function additionalClasses()
-    {
-        return false;
-    }
-
-    static function projectManagement()
-    {
-        return ['looksGood' => 'yes', 'optionsGood' => 'yes', 'optionsWork' => 'yes'];
-    }
-
-    static function propertyPathsToWhitelistInFlatProps()
-    {
-        return false;
-    }
-
-    static function propertyPathsToSsrElementWhenValueChanges()
-    {
-        return false;
-    }
-}
+            "rows", "Rows", [
+                c("background_a", "Background A", [], ['type' => 'color', 'layout' => 'inline', 'colorOptions' => ['type' => 'solidAndGradient']]),
+                c("background_b", "Background B", [], ['type' => 'color', 'layout' => 'inline', 'colorOptions' => ['type' => 'solidAndGradient']]),
+                getPresetSection("EssentialElements\\spacing_padding_all", "Padding", "padding"),
+                c("border", "Border", [], ['type' => 'toggle', 'layout' => 'inline']),
+                c("border_color", "Border Color", [], ['type' => 'color', 'layout' => 'inline'])
+            ],
+            ['type' => 'section']
+        ),
+        c("typography", "Typography", [
+            getPresetSection("EssentialElements\\typography", "Title", "title"),
+            getPresetSection("EssentialElements\\typography", "Price", "price"),
+            getPresetSection("EssentialElements\\typography", "Description", "description")
+        ], ['type' => 'section']),
+        c("container", "Container", [
+            c("width", "Width", [], ['type' => 'unit', 'layout' => 'inline'], true),
+            c("background", "Background", [], ['type' => 'color', 'layout' => 'inline', 'colorOptions' => ['type' => 'solidAndGradient']]),
+            getPresetSection("Ess
